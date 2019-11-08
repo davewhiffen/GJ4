4dd989d3691e4dd6972c4acb1ec0b4d077130653
Dave Whiffen <david.whiffen@ryerson.ca>
2019-11-08 16:36:22 -0500

    Blowing up the Levell

--- Assets/RedHealth.cs
+++ Assets/RedHealth.cs
@@ -7,7 +7,7 @@ public class RedHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RedHealth = maxHealth
+     
     }
 
     // Update is called once per frame
